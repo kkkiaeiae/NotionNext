f233069c0c76da0faddcf75d3adbc258e6864411
Ербол Елқұт <kjgtfbtb@gmail.com>
2026-03-21 20:54:03 +0500

    Modify avatar and home banner image in blog config
    Updated avatar and home banner image URLs in the configuration.

--- blog.config.js
+++ blog.config.js
@@ -1,20 +1,16 @@
 const NOTION_CONFIG = {
   PSEUDO_STATIC: false,
-  
   TITLE: 'Elkut的赛博次元档案馆',
   DESCRIPTION: '这里是Elkut的私人观测站，记录笔记、ACG、TRPG与哲学。✨',
   KEYWORDS: 'Notion, 博客, ACG, 二次元, 编程, AI, Elkut, TRPG, 哲学',
   LANG: 'zh-CN',
   SINCE: 2024,
-  
   IS_SITEMAP_AUTO_GENERATED: true,
-
-  AVATAR: 'https://ibb.co.com/bMrg1Xvg',
+  AVATAR: 'https://i.ibb.co.com/zTShsZ2h/Kenshi-Yonezu-2019-cr-Tomokazu-Yamada-billboard-1548.webp',
   AVATAR_LINK: '/about',
   NICKNAME: 'Elkut',
   BIO: ' 👾 ',
-
-  HOME_BANNER_IMAGE: 'https://ibb.co.com/9HkhcDKs',
+  HOME_BANNER_IMAGE: 'https://i.ibb.co.com/XxrDjQ0S/80f1e224aba27c7c3b9c1bbff12750b2.jpg',
   HOME_BANNER_STRINGS: [
     '~ Welcome to Elkut\'s Digital Sanctuary ~',
     '这里记录着关于 <b class="text-orange-300">人工智能</b> 的课程笔记...',
@@ -22,20 +18,18 @@ const NOTION_CONFIG = {
     '偶尔还会讨论 <b class="text-green-300">独立游戏</b> 与哲学命题...',
     '愿你在我的次元里找到共鸣 ✨'
   ],
-
   MENU_CATEGORY: true,
   MENU_TAG: true,
   MENU_ARCHIVE: true,
   MENU_SEARCH: true,
-
   CONTACT_EMAIL: '',
   CONTACT_GITHUB: '',
   CONTACT_TWITTER: '',
   CONTACT_TELEGRAM: '',
   CONTACT_BILIBILI: '',
-
   BEI_AN_INFO: '',
   BEI_AN_LINK: 'https://beian.miit.gov.cn/',
-
   THEME: 'hexo'
 }
+
+module.exports = NOTION_CONFIG
